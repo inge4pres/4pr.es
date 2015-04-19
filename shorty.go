fc82e4c5e981d7543ace126bddd4651d4d1dbe87
inge4pres <fgualazzi@gmail.com>
2015-04-19 19:16:00 +0200

    Version RC-1.0

--- shorty.go
+++ shorty.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-martini/martini"
 	_ "github.com/go-sql-driver/mysql"
 	"math/rand"
+	"net/http"
 	"time"
 )
 
@@ -16,7 +17,7 @@ var letters = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV
 var coder Coder
 
 type Coder struct {
-	Length    uint `6`
+	Length    uint
 	Shrt, Url string
 }
 
@@ -26,51 +27,69 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+	coder.Length = 8
 }
 
 func main() {
 	m := martini.Classic()
-	//Insert
-	m.Get("/", func() {
-		m.Use(render.Renderer(render.Options{
-			Layout:  "index",
-			Charset: "UTF-8",
-		}))
-
+	//Landing page
+	m.Use(render.Renderer(render.Options{
+		Layout:     "index",
+		Extensions: []string{".tmpl", ".html"},
+		Charset:    "UTF-8",
+	}))
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "", "")
 	})
 	//Create entry for shortened URL
-	m.Post("/", func() {
-		createUrl()
-		m.Use(render.Renderer(render.Options{
-			Layout:  "post",
-			Charset: "UTF-8",
-		}))
+	m.Post("/", func(r *http.Request) string {
+		short, err := createUrl(r.FormValue("url"))
+		if err != nil {
+			return "Error shortening URL " + r.FormValue("url") + "\n" + err.Error()
+		}
+		return "Short URL: " + short + " \n"
 	})
 	//Redirection to original URL
-	m.Get("/:id", func(params martini.Params) {
-		redirectToUrl(params["id"])
+	m.Get("/:short", func(params martini.Params, w http.ResponseWriter, r *http.Request) string {
+		redir, err := getUrl(params["short"])
+		if err != nil {
+			return "404 :(\nURL " + params["short"] + " not found\n"
+		}
+		http.Redirect(w, r, redir, 301)
+		return "Redirect OK :D"
 	})
 
-	fmt.Println("Serving on localhost:3000")
-	m.Run()
+	fmt.Println("Shortening URLS on localhost:1337")
+	m.RunOnAddr(":1337")
 }
 
-func createUrl(input string) (string, int, error) {
+func createUrl(input string) (string, error) {
 	coder.Url = input
-	coder.Shorten()
-	var id int
-	err := db.QueryRow("INSERT INTO urls (url) VALUES ($1) returning id", url).Scan(&id)
+	coder.Shrt = Shorten(coder.Length)
+	/*_ = db.QueryRow("SELECT id from ursl where short = $1", coder.Shrt).Scan(&id)
+		fmt.Printf("Short URL %s is alredy gone!", coder.Shrt)
+	} */
+	_, err := db.Exec("INSERT INTO urls VALUES (null, ?, ?, null)", coder.Url, coder.Shrt)
 	if err != nil {
-		return "", 0, err
+		return "", err
 	}
-	return coder.Shrt, id, nil
+	return coder.Shrt, nil
 }
 
-func (c *Coder) Shorten() {
+func getUrl(short string) (string, error) {
+	var redir string
+	err := db.QueryRow("SELECT url FROM urls WHERE short = ?", short).Scan(&redir)
+	if err != nil {
+		return "", err
+	}
+	return redir, nil
+}
+
+func Shorten(c uint) string {
 	rand.Seed(time.Now().UnixNano())
-	b := make([]rune, c.Length)
+	b := make([]rune, c)
 	for i := range b {
 		b[i] = letters[rand.Intn(len(letters))]
 	}
-	c.Shrt = string(b)
+	return string(b)
 }
