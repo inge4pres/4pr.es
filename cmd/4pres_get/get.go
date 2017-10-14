0bce06dc87a303942e2bc1ff42194984894bbdf5
Francesco Gualazzi <francesco.gualazzi@lastminute.com>
2017-10-14 21:26:57 +0200

    creating pkg folder as go convention

--- cmd/4pres_get/get.go
+++ cmd/4pres_get/get.go
@@ -5,7 +5,7 @@ import (
 	"log"
 
 	"github.com/eawsy/aws-lambda-go/service/lambda/runtime"
-	"github.com/inge4pres/4pr.es/shortener"
+	"github.com/inge4pres/4pr.es/pkg/shortener"
 )
 
 var domain = "4pr.es"
